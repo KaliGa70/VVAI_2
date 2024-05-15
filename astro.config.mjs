71cdfe43520c9a65cbf3b858541b17179e12d774
Angel Andres Garcia <1andresga07@gmail.com>
2024-05-14 22:00:30 -0600

    Se agregó Astro DB y node js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), db()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
