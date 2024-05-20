b5e0eda5d278107d1943d37648294f99dca879e6
Angel Andres Garcia <1andresga07@gmail.com>
2024-05-20 02:13:59 -0600

    se agregó una tabla y datos para la base de datos, y se agregó astrodb

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,15 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Usuarios = defineTable({
+  columns:{
+    id: column.number({primaryKey: true, autoIncrement: true}), 
+    nombreUsuario: column.text(),//Combinacion del nombre ejemplo: aagarcia
+    contrasena: column.text(),
+    rol: column.text(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
-});
+  tables: { Usuarios },
+})
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { db, Usuarios } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+	db.insert(Usuarios).values([{
+		nombreUsuario: "aagarcia",
+		contrasena: "123",
+		rol: "admin",
+	},
+	{
+		nombreUsuario: "dpalacios",
+		contrasena: "456",
+		rol: "admin",
+	},]);
 }
